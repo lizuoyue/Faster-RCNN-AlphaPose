1f23c05af68924553d6046d4e5b55a9b3156fa45
lizuoyue <li.zuoyue@foxmail.com>
2018-07-27 15:42:27 +0200

    a

--- tf-test.py
+++ tf-test.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
 import numpy as np
+import time
 
 a = tf.placeholder(tf.float32, [500, 1000])
 b = tf.placeholder(tf.float32, [1000, 300])
 c = tf.matmul(a, b)
 
 with tf.Session() as sess:
-	for i in range(100):
+	t = time.time()
+	for i in range(1000):
 		d = sess.run(c, feed_dict = {a: np.ones((500, 1000)), b: np.ones((1000, 300))})
 		print(i, d.shape)
+	print(time.time() - t)
