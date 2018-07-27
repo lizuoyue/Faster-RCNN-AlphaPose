9e524abe7dba51e9e8429bbac9061cec859bddaa
lizuoyue <li.zuoyue@foxmail.com>
2018-07-27 15:40:17 +0200

    a

--- tf-test.py
+++ tf-test.py
@@ -6,6 +6,6 @@ b = tf.placeholder(tf.float32, [1000, 300])
 c = tf.matmul(a, b)
 
 with tf.Session() as sess:
-	for i in range(10000):
+	for i in range(100):
 		d = sess.run(c, feed_dict = {a: np.ones((500, 1000)), b: np.ones((1000, 300))})
-		print(i, d)
+		print(i, d.shape)
