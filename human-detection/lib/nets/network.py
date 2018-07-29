ce18d61581999c186eeb3f530df221995e3ec0fb
lizuoyue <li.zuoyue@foxmail.com>
2018-07-29 21:11:33 +0200

    a

--- human-detection/lib/nets/network.py
+++ human-detection/lib/nets/network.py
@@ -227,12 +227,10 @@ class Network(object):
 
     self._score_summaries.update(self._predictions)
 
-    for v in tf.global_variables():
-      print(v.name)
-    print('=================')
-    for v in tf.trainable_variables():
-      print(v.name)
-    quit()
+    # for v in tf.global_variables():
+    #   print(v.name)
+    # for v in tf.trainable_variables():
+    #   print(v.name)
 
     return rois, cls_prob, bbox_pred
 
