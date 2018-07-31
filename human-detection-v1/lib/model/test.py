02a36f0779a23cb981722bf2a50d0e2b4fb83e26
lizuoyue <li.zuoyue@foxmail.com>
2018-07-31 11:07:19 +0200

    a

--- human-detection-v1/lib/model/test.py
+++ human-detection-v1/lib/model/test.py
@@ -33,7 +33,7 @@ def _get_image_blob(im,scale):
       in the image pyramid
   """
   im_orig = im.astype(np.float32, copy=True)
-  im_orig -= cfg.PIXEL_MEANS
+  im_orig -= cfg.PIXEL_MEANS[..., 0: 3]
 
   im_shape = im_orig.shape
   im_size_min = np.min(im_shape[0:2])
