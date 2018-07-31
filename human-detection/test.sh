c201907a6edae16718794971d16a47e3c8190787
lizuoyue <li.zuoyue@foxmail.com>
2018-07-31 23:55:52 +0200

    a

--- human-detection/test.sh
+++ human-detection/test.sh
@@ -1,8 +1,8 @@
 set_time=$2
 while true; do
-	time='date +%Y%m%d%H%M%S'
-	if [ $time -ge $set_time ]; then
+	time="date +%Y%m%d%H%M%S"
+	if [ "$time" -ge "$set_time" ]; then
 		break
 	fi
 done
-echo 'dashabi'
+echo "dashabi"
