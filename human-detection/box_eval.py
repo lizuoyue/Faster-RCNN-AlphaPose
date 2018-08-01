a98fb608801acf6f43f7279a12b676b2baf4bdfb
lizuoyue <li.zuoyue@foxmail.com>
2018-08-01 10:24:59 +0200

    a

--- human-detection/box_eval.py
+++ human-detection/box_eval.py
@@ -7,23 +7,17 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 	d = h5py.File('tools/prediction/BBOX/test-bbox.h5')
 	boxes = [item for item in zip(d['xmin'], d['ymin'], d['xmax'], d['ymax'])]
-	box_scores = [[float(item) for item in line.strip().split()] for line in open('tools/prediction/BBOX/score-proposals.txt').readlines()]
-	choose = 'all box'
-	if choose == 'final box':
-		boxes = [boxes[int(line.strip())] for line in open('finalBoxIdx.txt').readlines()]
-		imgIds = [int(item.strip().split('\t')[0].replace('.jpg', '')) for item in open('../POSE/scores.txt').readlines()]
-	if choose == 'all box':
-		imgIds = [int(item.strip().split('\t')[0].replace('.jpg', '')) for item in open('tools/prediction/BBOX/test-images.txt').readlines()]
-	assert(len(imgIds) == len(boxes))
+	box_scores = [float(line.strip()) for line in open('tools/prediction/BBOX/score-proposals.txt').readlines()]
+	assert(len(boxes) == len(box_scores))
+	imgIds = [int(item.strip().split('/')[-1].replace('.jpg', '')) for item in open('tools/prediction/BBOX/test-images.txt').readlines()]
+	assert(len(boxes) == len(imgIds))
 
 	res = [{
 		'image_id'    : imgId,
 		'category_id' : 1,
 		'bbox'        : [x1, y1, x2 - x1, y2 - y1],
-		'score'       : 1,
-	} for imgId, (x1, y1, x2, y2) in zip(imgIds, boxes)]
-
-	assert(len(res) == len(box_scores))
+		'score'       : score
+	} for imgId, (x1, y1, x2, y2), score in zip(imgIds, boxes, box_scores)]
 	print(len(res))
 
 	gtCoco = COCO('person_keypoints_val2017.json')
