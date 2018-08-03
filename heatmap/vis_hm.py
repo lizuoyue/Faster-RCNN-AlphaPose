c88cf10c8ecc298af5d547476f7614a82fcc60cf
lizuoyue <li.zuoyue@foxmail.com>
2018-08-03 23:04:03 +0200

    a

--- heatmap/vis_hm.py
+++ heatmap/vis_hm.py
@@ -12,7 +12,7 @@ idxs = [0, 1, 2, 3, 4, 2, 3, 4, 5, 6, 7, 5, 6, 7, 8, 8, 9, 9]
 
 from pycocotools.coco import COCO
 import numpy as np
-import skimage
+from skimage import io
 
 for dataset in ['val2017', 'train2017']:
 	coco = COCO('../../coco2017data/annotations/instances_%s.json' % dataset)
@@ -28,11 +28,11 @@ for dataset in ['val2017', 'train2017']:
 			for x, y, v in part:
 				if v < 600:
 					continue
-				hm[j] = np.maximum(hm[j], -((xx - x) ** 2 + (yy - y) ** 2) / 100 + np.log(v / 6000))
+				hm[j] = np.maximum(hm[j], -((xx - x) ** 2 + (yy - y) ** 2) / 100.0 + np.log(v / 6000.0))
 			hm[j] = np.exp(hm[j])
 		hm_c = np.zeros((3, h, w))
 		for j in range(3):
 			for k in range(18):
 				hm_c[j] += hm[k] * colors[idxs[k]][j]
-		hm_c = np.array(np.maximum(np.minimum(hm_c, 255), 0), np.float32).transpose([1, 2, 0])
-		skimage.io.imsave('/disks/data4/zyli/Faster-RCNN-AlphaPose/heatmap/%s/%s.png' % (dataset, imgIdStr), hm_c)
+		hm_c = np.array(np.maximum(np.minimum(hm_c, 255), 0), np.uint8).transpose([1, 2, 0])
+		io.imsave('/disks/data4/zyli/Faster-RCNN-AlphaPose/heatmap/%s/%s.png' % (dataset, imgIdStr), hm_c)
