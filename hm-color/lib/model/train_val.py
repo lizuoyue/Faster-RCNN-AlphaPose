1654746bec905f66d81d58484b8fa26a7996bd46
lizuoyue <li.zuoyue@foxmail.com>
2018-08-04 11:41:47 +0200

    a

--- hm-color/lib/model/train_val.py
+++ hm-color/lib/model/train_val.py
@@ -200,7 +200,9 @@ class SolverWrapper(object):
     variables = tf.global_variables()
     # Initialize all variables first
     sess.run(tf.variables_initializer(variables, name='init'))
-    var_keep_dic = self.get_variables_in_checkpoint_file(self.pretrained_model,exclusions=['resnet_v1_152/bbox_pred','resnet_v1_152/cls_score','resnet_v1_152/rpn_cls_score','resnet_v1_152/rpn_bbox_pred'])
+    var_keep_dic = self.get_variables_in_checkpoint_file(self.pretrained_model,
+      exclusions=[]#['resnet_v1_152/bbox_pred','resnet_v1_152/cls_score']
+    )
     # Get the variables to restore, ignoring the variables to fix
     variables_to_restore = self.net.get_variables_to_restore(variables, var_keep_dic)
 
@@ -278,6 +280,8 @@ class SolverWrapper(object):
       rate, last_snapshot_iter, stepsizes, np_paths, ss_paths = self.restore(sess, 
                                                                             str(sfiles[-1]), 
                                                                             str(nfiles[-1]))
+    self.snapshot(sess, 0)
+    quit()
     timer = Timer()
     iter = last_snapshot_iter + 1
     last_summary_time = time.time()
