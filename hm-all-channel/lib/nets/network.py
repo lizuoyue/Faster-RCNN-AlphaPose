2334dcbf88fc05fd8cd4b1879135a7ada5f1ad0f
lizuoyue <li.zuoyue@foxmail.com>
2018-08-10 16:31:23 +0200

    a

--- hm-all-channel/lib/nets/network.py
+++ hm-all-channel/lib/nets/network.py
@@ -48,10 +48,9 @@ class Network(object):
     # use a customized visualization function to visualize the boxes
     if self._gt_image is None:
       self._add_gt_image()
-    gt_hm = tf.tile(tf.expand_dims(tf.reduce_max(self._gt_image[..., 3:], axis = -1), axis = -1), [1, 1, 1, 3])
-    print(gt_hm.shape)
+    gt_hm = tf.tile(tf.expand_dims(tf.reduce_max(self._gt_image[..., :-3], axis = -1), axis = -1), [1, 1, 1, 3])
     image = tf.py_func(draw_bounding_boxes, 
-                      [self._gt_image[..., :3], self._gt_boxes, self._im_info],
+                      [self._gt_image[..., -3:], self._gt_boxes, self._im_info],
                       tf.float32, name="gt_boxes")
     image_hm = tf.py_func(draw_bounding_boxes, 
                       [gt_hm, self._gt_boxes, self._im_info],
